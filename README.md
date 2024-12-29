1e70e593de3eaed6d622665ead9755d3e72b46f4
Albert Phu <127133346+TTerablade@users.noreply.github.com>
2024-12-28 18:33:49 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Project info
 
-**URL**: https://lovable.dev/projects/7afb5d6b-9115-475b-9a3e-37cc164c009f
+**URL**: https://lovable.dev/projects/c53d8b46-992b-48ed-abda-f1c975cc38c0
 
 ## How can I edit this code?
 
@@ -10,7 +10,7 @@ There are several ways of editing your application.
 
 **Use Lovable**
 
-Simply visit the [Lovable Project](https://lovable.dev/projects/7afb5d6b-9115-475b-9a3e-37cc164c009f) and start prompting.
+Simply visit the [Lovable Project](https://lovable.dev/projects/c53d8b46-992b-48ed-abda-f1c975cc38c0) and start prompting.
 
 Changes made via Lovable will be committed automatically to this repo.
 
@@ -62,7 +62,7 @@ This project is built with .
 
 ## How can I deploy this project?
 
-Simply open [Lovable](https://lovable.dev/projects/7afb5d6b-9115-475b-9a3e-37cc164c009f) and click on Share -> Publish.
+Simply open [Lovable](https://lovable.dev/projects/c53d8b46-992b-48ed-abda-f1c975cc38c0) and click on Share -> Publish.
 
 ## I want to use a custom domain - is that possible?
 
--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,33 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { RecipeProvider } from "./contexts/RecipeContext";
+import Home from "./pages/Home";
+import FindRecipe from "./pages/FindRecipe";
+import RecipeCollection from "./pages/RecipeCollection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Download from "./pages/Download";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
+      <RecipeProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/find-recipe" element={<FindRecipe />} />
+            <Route path="/recipe-collection" element={<RecipeCollection />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/download" element={<Download />} />
+          </Routes>
+        </BrowserRouter>
+      </RecipeProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
