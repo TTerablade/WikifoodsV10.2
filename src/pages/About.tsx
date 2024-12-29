ec2230d0204f9f58d968232e0a02848d9da18dca
Albert Phu <127133346+TTerablade@users.noreply.github.com>
2024-12-29 10:03:42 -0600

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -38,8 +38,7 @@ const About = () => {
               <h3 className="text-xl font-bold mb-4">For TSA</h3>
               <p className="text-gray-600">
                 Our Files Are Linked Here.
-                Credits To Our Team:
-                Poeple
+                Credits To Our Team:Popel
               </p>
             </div>
           </div>
